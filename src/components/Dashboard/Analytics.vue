e20ee28b1d92d1759e49c6eb3f31997c9a768a7d
Allen <glux7381@gmail.com>
2019-07-11 17:28:36 +0800

    Edit the analytics page

--- src/components/Dashboard/Analytics.vue
+++ src/components/Dashboard/Analytics.vue
@@ -1,5 +1,66 @@
 <template>
-    <h1>
-        Analytics
-    </h1>
+  <v-container class="pa-0">
+    <v-layout column class="pa-0">
+      <v-flex xs6 class="pa-0">
+        <div class="todolist-title">
+          <h2 class="analytics__title">Focus Time</h2>
+        </div>
+      </v-flex>
+      <v-flex class="pa-0 mt-3">
+        <h2 class="analytics__title">Today</h2>
+        <v-layout class="analytics-data">
+          <h3 class="analytics-data__number">
+            20
+            <span class="analytics-data__unit">/Tomato</span>
+          </h3>
+        </v-layout>
+      </v-flex>
+      <v-flex class="pa-0">
+        <h2 class="analytics__title">Week</h2>
+        <v-layout class="analytics-data">
+          <h3 class="analytics-data__number">
+            108
+            <span class="analytics-data__unit">/Tomato</span>
+          </h3>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+    <v-layout column class="pa-0">
+      <v-flex xs6>
+        <div class="todolist-title">
+          <h2 class="analytics__title">Week</h2>
+        </div>
+      </v-flex>
+      <v-flex xs6>
+        <h1>123132</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+  <!-- <div>
+
+    <v-layout class="analytics">
+      <v-flex class="pa-0 test">
+        <h2 class="analytics__title">Today</h2>
+        <v-layout class="analytics-data">
+          <h3 class="analytics-data__number">
+            20
+            <span class="analytics-data__unit">/Tomato</span>
+          </h3>
+        </v-layout>
+      </v-flex>
+      <v-flex class="pa-0">
+        <h2 class="analytics__title">Week</h2>
+        <v-layout class="analytics-data">
+          <h3 class="analytics-data__number">
+            108
+            <span class="analytics-data__unit">/Tomato</span>
+          </h3>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+
+    <div class="todolist-title mt-5">
+      <h2 class="todolist-title__content">chart</h2>
+    </div>
+  </div>-->
 </template>
--- src/components/Dashboard/Ringtones.vue
+++ src/components/Dashboard/Ringtones.vue
@@ -1,5 +1,10 @@
 <template>
-    <h1>
-        Ringtones
-    </h1>
+  <div>
+    <div class="todolist-title">
+      <h2 class="todolist-title__content">work</h2>
+    </div>
+    <div class="todolist-title mt-5">
+      <h2 class="todolist-title__content">break</h2>
+    </div>
+  </div>
 </template>
